d484c5112a094392fe5ce19d3e3c16a91fd5f870
pxn-nauxray <nauxray@phantom.sh>
2022-12-05 06:54:37 +0800

    feat: finish card UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "./Card.css";
+import { SlStar } from "react-icons/sl";
 
 export default function Card({ yarn }) {
   return (
     <div className="card-container">
-      {yarn.average_rating}
-      <span>{yarn.name}</span>
-      <span>{yarn.brand.name}</span>
-      <span>materials</span>
-      <span>{yarn.average_rating}</span>
-      <span>{yarn.reviewCount}</span>
+      {yarn.img_url?.length > 0 && (
+        <div className="card-image-container">
+          <img className="card-image" src={yarn.img_url[0]} alt={yarn.name} />
+        </div>
+      )}
+      <div className="card-details">
+        <div className="yarn-name">{yarn.name}</div>
+        <div className="card-section">
+          <span>{yarn.brand.name}</span>
+          <div className="yarn-rating">
+            <span>
+              {parseFloat(yarn.average_rating)
+                .toFixed(1)
+                .replace(/[.,]0$/, "")}
+              /5
+            </span>
+            <SlStar size={18} />
+          </div>
+        </div>
+
+        <div className="card-section">
+          <span>materials</span>
+          <span>
+            {yarn.reviewCount} review{yarn.reviewCount === 1 ? "" : "s"}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
